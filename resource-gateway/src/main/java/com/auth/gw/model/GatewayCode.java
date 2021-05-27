a526b6e1d9424433087cdbc1e752024aec8cca1d
Dean <dean.wu@rhf-tech.com>
2021-05-27 15:10:05 +0800

    init

--- resource-gateway/src/main/java/com/auth/gw/model/GatewayCode.java
+++ resource-gateway/src/main/java/com/auth/gw/model/GatewayCode.java
@@ -1,11 +1,11 @@
 package com.auth.gw.model;
 
 public enum GatewayCode {
-    SUCCESS(200, "操作成功"),
-    FAILED(500, "操作失败"),
-    VALIDATE_FAILED(404, "参数检验失败"),
-    UNAUTHORIZED(401, "暂未登录或token已经过期"),
-    FORBIDDEN(403, "没有相关权限");
+    SUCCESS(200, "success"),
+    FAILED(500, "error"),
+    VALIDATE_FAILED(404, "request filed error"),
+    UNAUTHORIZED(401, "token error"),
+    FORBIDDEN(403, "not auth");
     private long code;
     private String message;
 
